e64b9f93fadd561a3f6f7c62995106f5ce1ea46a
paet_jantima <younana121@gmail.com>
2022-08-07 10:36:56 +0700

    รอดไป

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:example2/screen/about_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String name = " Bundit nan";
+  int age = 35;
+  bool lookingGood = true;
+
+  List<String> myDognames = ["loogtoo", "paveena", "maik", "Paravit"];
+  Map<String, dynamic> user = {
+    "id": 1,
+    "name": "John Doe",
+    "sex": "male",
+    "married": false
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +30,28 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("HomeScreen"),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return AboutScreen();
-                },
-              ),
-            )
-          },
-          child: Text("Go to about screen"),
+        child: Column(
+          children: [
+            Text("สวัสดีฉันชื่อ $name"),
+            Text("ฉัมีอายุ $age"),
+            Text("ฉันหล่อมากเปนเรื่อง ${lookingGood ? "จริง" : "ไม่จริง"}"),
+            Text("หมฉันชื่อ: ${myDognames[0] } , ${myDognames[1]}, ${myDognames[2]}, และ ${myDognames[3]} "),
+            ...myDognames.map((item) => Text(item)).toList(),
+            Text("เพื่อนฉันชื่อ : ${user["name"]}"),
+            Text("แล้วเขาก็ : ${user["married"] ? "แต่งงานแล้ว" : "ยังไม่ได้แต่งงาน"}"),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AboutScreen();
+                    },
+                  ),
+                )
+              },
+              child: Text("Go to about screen"),
+            ),
+          ],
         ),
       ),
     );
